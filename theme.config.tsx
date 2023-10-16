ccc0178653fa0ee9fe760819955b332edc64aabc
Louie Heaton <louieheaton@icloud.com>
2023-10-16 14:27:43 +0100

    init

---
+++ theme.config.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Oberon</span>,
+  project: {
+    link: 'https://github.com/SoBold/oberon',
+  },
+  docsRepositoryBase: 'https://github.com/SoBold/oberon-docs',
+  footer: {
+    text: 'Oberon Docs | SoBold',
+  },
+}
+
+export default config
