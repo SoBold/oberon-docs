d3745837c0b2470698c31e1bf4652d4bd62f3882
soboldsantosh <santosh@soboldltd.com>
2025-05-06 10:56:00 +0100

    check link in mdx

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,24 +1,3 @@
-// import React from 'react'
-// import { DocsThemeConfig } from 'nextra-theme-docs'
-
-// const config: DocsThemeConfig = {
-//   logo: <span>Oberon</span>,
-//   useNextSeoProps() {
-//     return {
-//       titleTemplate: '%s - Oberon'
-//     }
-//   },
-//   project: {
-//     link: 'https://github.com/SoBold/oberon',
-//   },
-//   docsRepositoryBase: 'https://github.com/SoBold/oberon-docs',
-//   footer: {
-//     text: 'Oberon Docs | SoBold',
-//   },
-// }
-
-// export default config
-
 import React from 'react';
 import { DocsThemeConfig } from 'nextra-theme-docs';
 
@@ -35,24 +14,6 @@ const config: DocsThemeConfig = {
     docsRepositoryBase: 'https://github.com/SoBold/oberon-docs',
     footer: {
         text: 'Oberon Docs | SoBold',
-    },
-    sidebar: {
-        component: () => (
-            <div style={{ marginTop: '2rem', padding: '1rem' }}>
-                <a
-                    href="https://sobold.github.io/oberon-api/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    style={{
-                        color: '#888',
-                        fontSize: '0.875rem',
-                        textDecoration: 'none',
-                    }}
-                >
-                    Migration from Nextra v3 →
-                </a>
-            </div>
-        ),
     },
 };
 
